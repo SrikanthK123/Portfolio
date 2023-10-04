3bc48de328ca62b2dcb503122c896030263b610d
SrikanthK123 <93971062+SrikanthK123@users.noreply.github.com>
2023-10-04 15:09:57 +0530

    change2

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
 
 function App() {
   return (
