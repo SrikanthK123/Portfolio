ff2a1a722eed57af42f47de6eeb989c3ac389d3a
SrikanthK123 <93971062+SrikanthK123@users.noreply.github.com>
2024-03-07 21:12:50 +0530

    fewchange

--- src/components/Education.js
+++ src/components/Education.js
@@ -13,8 +13,8 @@ const Education = () => {
             <h1 class="text-body-emphasis">
               <i class="fa fa-graduation-cap text-white " aria-hidden="true" data-aos="zoom-in"></i>
             </h1>
-            <h3 data-aos="zoom-in" className="text-white" >Bachelor of Technology</h3>
-            <p class="lead text-white" data-aos="zoom-in" >
+            <h3 data-aos="zoom-in" className="text-white" style={{fontWeight:'600',textTransform:'uppercase',fontSize:'17px',padding:'10px'}} >Bachelor of Technology</h3>
+            <p class="lead text-white" data-aos="zoom-in"  style={{fontWeight:'600',textTransform:'uppercase',fontSize:'14px',padding:'10px'}} >
               I've completed my bachelor's degree in Computer Science and
               Engineering at Aurora’s Technological & Research Institute,
               Hyderabad<code style={{color:'rgb(52 171 236)',fontWeight:'bold'}}>(2022)</code>
@@ -27,8 +27,8 @@ const Education = () => {
             <h1 class="text-body-emphasis">
               <i class="fa fa-university text-white" aria-hidden="true" data-aos="zoom-in"></i>
             </h1>
-            <h3 data-aos="zoom-in" className="text-white">Completion of Board of Intermediate Public Examination</h3>
-            <p class="lead text-white"  data-aos="zoom-in" >
+            <h3 data-aos="zoom-in" className="text-white" style={{fontWeight:'600',textTransform:'uppercase',fontSize:'17px',padding:'10px'}}>Completion of Board of Intermediate Public Examination</h3>
+            <p class="lead text-white"  data-aos="zoom-in" style={{fontWeight:'600',textTransform:'uppercase',fontSize:'14px',padding:'10px'}} >
               I've completed my Higher Secondary School Certificate (HSSC) in
               Maths, Physics and Chemistry (MPC) at Sri Chaitanya Junior
               Kalasala <code style={{color:'rgb(52 171 236)',fontWeight:'bold'}}>(2018)</code>
@@ -41,8 +41,8 @@ const Education = () => {
             <h1 class="text-body-emphasis">
               <i className="fas fa-school text-white" data-aos="zoom-in"></i>
             </h1>
-            <h3 data-aos="zoom-in" className="text-white" > Completion of Board of Secondary School Certificate </h3>
-            <p class="lead text-white" id="edubox" data-aos="zoom-in" >
+            <h3 data-aos="zoom-in" className="text-white" style={{fontWeight:'600',textTransform:'uppercase',fontSize:'17px',padding:'10px'}} > Completion of Board of Secondary School Certificate </h3>
+            <p class="lead text-white" id="edubox" data-aos="zoom-in"  style={{fontWeight:'600',textTransform:'uppercase',fontSize:'14px',padding:'10px'}}>
               I've completed my Secondary School Certificate (SSC) at Care Model
               High School<code style={{color:'rgb(52 171 236)',fontWeight:'bold'}}>(2016)</code>
             </p>
